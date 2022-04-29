cf70b61acb438db825cf4e80bc6c790694a75065
Omri Shahar <omrisr@mta.ac.il>
2022-04-29 12:56:38 +0300

    Add project files.

---
+++ Ex03.ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Ex03.ConsoleUI;
+using Ex02.ConsoleUtils;
+
+namespace Ex03.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
