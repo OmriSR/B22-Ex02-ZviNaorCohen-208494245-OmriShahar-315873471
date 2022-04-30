e1b8e1b828877656665a196eb8d5d3447ef55dd9
Omri Shahar <omrisr@mta.ac.il>
2022-04-30 14:19:34 +0300

    Vehicle class created

---
+++ Ex03.GarageLogic/Vehicel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    abstract class Vehicel
+    {
+        string m_Modle;
+        string m_LicenseNumber;
+        float m_EnergyLeft;
+        Wheel[] m_Wheels;
+        
+    }
+}
